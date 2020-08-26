8c5ee4fffbe5323848fca92cf93878ee079dbd58
Sosuke Brause <brosuke89@gmail.com>
2020-08-26 16:09:41 -0700

    first commit with auth function

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
-import { InputBase } from '@material-ui/core';
-import car from './images/pic.png'
-
+import React from "react";
+import "./App.css";
+import { InputBase } from "@material-ui/core";
+// import Car from "./images/apoc-1.jpg";
 
 function App() {
   return (
-    <div className="App" styles={{ backgroundImage:`url(${pic})` }}>
-<InputBase/>
+    <div>
+      <h1>Hello Alert App</h1>
+      <InputBase />
+      {/* <Car /> */}
     </div>
   );
 }
