886dd8215f9f4a188f30deb669d7fe6046138fb6
Sosuke Brause <brosuke89@gmail.com>
2020-09-01 19:40:35 -0700

    committing to enable pull req to merge jul to mine

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Form from "../card/AuthPost";
 import Danger from "../Danger";
 import Search from "./Search";
 import { useUserContext } from "../context/UserContext";
+
+const data = [
+  { argument: 1, value: 10 },
+  { argument: 2, value: 20 },
+  { argument: 3, value: 30 },
+];
+
 const Home = () => {
+  const [covdData, setCovdData] = useState([]);
   const { userData } = useUserContext();
   console.log(userData);
   return (
