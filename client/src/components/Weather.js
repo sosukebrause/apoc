fbb916d0942942ecb396cd013dfd8ea57afb9ae6
julhanlon <jhanlon289@gmail.com>
2020-09-04 23:17:37 -0700

    fix

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -7,6 +7,8 @@ const Weather = (props) => {
         <div>
             <h3> Temp: {props.weatherObj.temp}</h3>
             <h3> UVI: {props.weatherObj.uvi}</h3>
+            <h3> Humidity: {props.weatherObj.humidity}</h3>
+            <h3> Wind_speed: {props.weatherObj.wind_speed}</h3>
         </div>
     )
 }
