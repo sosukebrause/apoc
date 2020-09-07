a0873068468cd389fe95bb8beaab444b7a3153f7
julhanlon <jhanlon289@gmail.com>
2020-09-07 10:58:11 -0700

    fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
     db: require('./dbController'),
     covid: require('./covidController'),
     weather: require('./weatherController'),
@@ -9,14 +8,3 @@ module.exports = {
     //crime: require('./crimeController'),
     earthquake: require('./earthquakeController'),
   }
-=======
-  db: require("./dbController"),
-  covid: require("./covidController"),
-  weather: require("./weatherController"),
-  air: require("./airController"),
-  map: require("./mapController"),
-
-  //crime: require('./crimeController'),
-  earthquake: require("./earthquakeController"),
-};
->>>>>>> 827dcc338a7cff7b0ff3c3f7dc1609c2d0c66754
